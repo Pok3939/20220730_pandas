494e773716750113b79aa1435b38f8de52e781bc
Pok3939 <105784621+Pok3939@users.noreply.github.com>
2022-07-30 13:19:49 +0800

    2nd commit

--- ted.py
+++ ted.py
@@ -1,2 +1,14 @@
 import pandas as pd
 import numpy as np
+ted = pd.read_csv("ted.csv")
+print(ted)
+
+print(ted.head(10))
+
+
+print(ted.dtypes)
+print(ted.sort_values('views').tail())
+ted['comments_per_view'] = ted.comments/ted.views
+print(ted.sort_values('comments_per_view').tail())
+print(ted.isna())
+print(ted.isna().sum())
